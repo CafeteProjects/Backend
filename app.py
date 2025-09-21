c1efe018273003790d83f8d528d9320be2001fe8
Bachelor_Thesis <tobias00@zedat.fu-berlin.de>
2025-09-21 14:24:55 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify
+import mysql.connector
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # erlaubt Flutter Zugriff von anderen Domains
+# MySQL-Verbindungsdaten, standen gott sei dank auf der Seite
+db_config = {
+    'host': 'ea5oe8.h.filess.io',       # dein Host
+    'user': 'cafete_topicglad',         # DB-Benutzer
+    'password': '15e8f9e8222cc351aa28ad15e3e46f8b98853c36',        # dein DB-Passwort
+    'database': 'cafete_topicglad',     # Name der DB
+    'port': 61001                       # Port von Filess
+}
+# API-Endpunkt für Menü
+@app.route('/menu', methods=['GET'])
+def get_menu():
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT itemname, price, category FROM menu")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(rows)  # liefert JSON zurück
+@app.route('/active', methods=['GET'])
+def get_active():
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT active FROM active")  # SQL-Query
+    rows = cursor.fetchall()                     # Ergebnis korrekt zuweisen
+    cursor.close()
+    conn.close()
+    return jsonify(rows)
+@app.route('/news', methods=['GET'])
+def get_news():
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT id, date, news,title FROM news")  # SQL-Query
+    rows = cursor.fetchall()                     # Ergebnis korrekt zuweisen
+    cursor.close()
+    conn.close()
+    return jsonify(rows)
+
+
+if __name__ == '__main__':
+    # host=0.0.0.0 damit von außen erreichbar
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+
