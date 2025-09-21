c393daa74ef79d35ce44c880fc112fe9fe1df4af
Bachelor_Thesis <tobias00@zedat.fu-berlin.de>
2025-09-21 14:43:01 +0200

    Flask für Render angepasst: dynamischer Port, Gunicorn, DB via Env

--- app.py
+++ app.py
@@ -45,6 +45,6 @@ def get_news():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # Render setzt PORT
     app.run(debug=True, host='0.0.0.0', port=port)
 
